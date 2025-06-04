4a7bff5f8390cd244a8575e9da36084f7b93bcf0
AmanIbrash <rabotnik460@gmail.com>
2025-06-04 14:43:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-TOKEN = os.getenv("API_TOKEN")
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я бот для заказов и исполнителей.")
 
-if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    await update.message.reply_text(f"Вы написали: {user_message}")
+
+app = ApplicationBuilder().token("ТВОЙ_ТОКЕН").build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+app.run_polling()
