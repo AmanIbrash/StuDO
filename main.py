598eef39409c5cd39b3f656a5770816165ba0756
AmanIbrash <rabotnik460@gmail.com>
2025-06-04 14:57:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
     await update.message.reply_text(f"Вы написали: {user_message}")
 
-app = ApplicationBuilder().token("ТВОЙ_ТОКЕН").build()
+app = ApplicationBuilder().token("7641233572:AAE-Zc5luR1OmPZuEAJ1okElewpkUXEVJ4c").build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
